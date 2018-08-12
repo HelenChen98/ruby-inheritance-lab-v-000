82591a8da65057ad6b7e5d41d9920f786d898df2
Karen C <chenyanhuan66@163.com>
2018-08-12 07:26:13 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -6,20 +6,4 @@ class User
     @last_name = last_name
   end
 
-  def first_name=(name)
-    @first_name = name
-  end
-
-  def self.first_name
-    @first_name
-  end
-
-  def last_name=(name)
-    @last_name = name
-  end
-
-  def self.last_name
-    @last_name
-  end
-
 end
